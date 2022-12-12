9ebc385b616bebd6691e22fdcf15494c29b1c725
dputhier <puthier@gmail.com>
2022-12-12 18:58:52 +0100

    added coord_brain description

---
+++ R/dataset_description.R
@@ -0,0 +1,16 @@
+#' The spot location of the brain dataset (see stxBrain in SeuratData).
+#'
+#' A dataset containing the x/y coordinates and class of 2696 Visium spots
+#'
+#' \itemize{
+#'   \item x. The x coordinates.
+#'   \item y. The y coordinates.
+#'   \item group. A factor. The groups of the spots infered using FindClusters() (Seurat library).
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name coord_brain
+#' @usage data(coord_brain)
+#' @format A data frame with 2696 rows and 4 variables
+NULL
